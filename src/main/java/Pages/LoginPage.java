6e69ba2ac5f4e91d6ad25dc68d090138781cb9c7
pratiksha <pratikshapatil0299@gmail.com>
2024-05-29 22:19:11 +0530

    pratikshas commit

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -1,5 +1,37 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
+	
+	public LoginPage(WebDriver driver) 
+	{
+		this.driver =driver;
+	}
+
+	
+	
+	WebDriver driver;
+	public void enterCrentials()
+	{
+		driver.findElement(By.xpath("//*[@name='username']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//*[@name=\'password\']")).sendKeys("admin123");
+	}
+
+	public void clickOnLogin()
+	{
+		driver.findElement(By.xpath("//*[@type='submit']")).click();
+		
+	}
+	public void enterPin()
+	{
+		driver.findElement(By.xpath("//*[@id='pin']")).sendKeys("866918");
+	}
+	public String getTitleofPage ()
+	{
+		String titleofpage = driver.getTitle();
+		return titleofpage;
+	}
 
 }
---
+++ src/test/java/Testclasses/Logout.java
@@ -0,0 +1,19 @@
+package Testclasses;
+
+public class Logout {
+	
+	  public void method()
+	  {
+		
+		System.out.println("HII");
+		
+	  }
+	  public void method2()
+	  {
+		
+		System.out.println("HII");
+		
+	  }
+
+
+}
